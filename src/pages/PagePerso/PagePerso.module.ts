fad9f8efc380e7f56b103c384495eceb7849bae7
romulus001 <31893216+romulus001@users.noreply.github.com>
2017-09-13 12:31:18 +0200

    Update PagePerso.module.ts

--- src/pages/PagePerso/PagePerso.module.ts
+++ src/pages/PagePerso/PagePerso.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
-import { HomePage } from '../home/home';
 import { SecondPage } from './PagePerso';
+
 @NgModule({
   declarations: [
-    HomePage,
     SecondPage
   ],
   imports: [
-    IonicModule.forRoot(HomePage)
+    IonicModule.forRoot(SecondPage)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    HomePage,
-    SecondPage
+     SecondPage
   ],
   providers: []
 })
-export class AppModule {}
+export class SecondPageModule {}
