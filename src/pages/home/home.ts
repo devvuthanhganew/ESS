3b68301d8690b0b87b0d09ae2b2cf2eb96d14ecc
romulus001 <31893216+romulus001@users.noreply.github.com>
2017-09-22 09:21:10 +0200

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {PagePersonnellePage} from '../page-personnelle/page-personnelle'
+import {Http} from '@angular/http';
 
 declare var atlan: any; 
 
